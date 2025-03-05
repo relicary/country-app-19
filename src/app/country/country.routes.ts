57175fcf697a478a6d5301803774e7e372dd93fe
Juan Cremades <relicary@gmail.com>
2025-03-05 15:37:43 +0100

    COMPONENT: Added a Layout Page and its children routes

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
+import { CountryLayoutPageComponent } from './layouts/country-layout-page/country-layout-page.component';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayoutPageComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
   }
 ]
 
---
+++ src/app/country/layouts/country-layout-page/country-layout-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-country-layout-page',
+  imports: [RouterOutlet],
+  templateUrl: './country-layout-page.component.html',
+})
+export class CountryLayoutPageComponent { }
