34dca686c6e03252b44e1cb2638724768c12df0e
Juan Cremades <relicary@gmail.com>
2025-03-05 17:34:12 +0100

    COMPONENT: Added CountryPage

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -3,6 +3,7 @@ import { CountryLayoutPageComponent } from './layouts/country-layout-page/countr
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 export const countryRoutes: Routes = [
   {
@@ -20,6 +21,10 @@ export const countryRoutes: Routes = [
       {
         path: 'by-region',
         component: ByRegionPageComponent
+      },
+      {
+        path: 'by/:code',
+        component: CountryPageComponent
       },
       {
         path: '**',
