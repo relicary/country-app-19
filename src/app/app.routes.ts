bd4806b0b6d6d5113698658392a82347ef3f761c
Juan Cremades <relicary@gmail.com>
2025-03-05 15:26:33 +0100

    ROUTES: Added basic route and loadChildren route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './country/pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes'), // Lazy Load
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
--- src/app/country/pages/home-page/home-page.component.ts
+++ src/app/country/pages/home-page/home-page.component.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   imports: [],
   templateUrl: './home-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent { }
