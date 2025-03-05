3084debe2b347e15ff30fab65cc1fd69e9525eb2
Juan Cremades <relicary@gmail.com>
2025-03-05 16:41:19 +0100

    COMPONENT: Added the CountryTopMenu to the CountryLayout

--- src/app/country/layouts/country-layout-page/country-layout-page.component.ts
+++ src/app/country/layouts/country-layout-page/country-layout-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopMenuComponent } from '../../components/top-menu/top-menu.component';
 
 @Component({
   selector: 'app-country-layout-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopMenuComponent],
   templateUrl: './country-layout-page.component.html',
 })
 export class CountryLayoutPageComponent { }
