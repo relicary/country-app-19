6dfa76a6ebeafd647fa577e53fd97b5057a5402b
Juan Cremades <relicary@gmail.com>
2025-03-05 15:42:37 +0100

    ROUTE: Routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './country/pages/home-page/home-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
 export const routes: Routes = [
   {
