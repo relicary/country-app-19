c6d315bd70c7216a8c0a5ad958984518a83c0f36
Juan Cremades <relicary@gmail.com>
2025-03-05 17:14:33 +0100

    COMPONENTS: Added ByCountryPage and ByRegionPage

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CountryLayoutPageComponent } from './layouts/country-layout-page/country-layout-page.component';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 
 export const countryRoutes: Routes = [
   {
@@ -10,6 +12,14 @@ export const countryRoutes: Routes = [
       {
         path: 'by-capital',
         component: ByCapitalPageComponent
+      },
+      {
+        path: 'by-country',
+        component: ByCountryPageComponent
+      },
+      {
+        path: 'by-region',
+        component: ByRegionPageComponent
       },
       {
         path: '**',
