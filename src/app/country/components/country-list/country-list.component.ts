91422219e121d3f626079844b01dcd3e590bbc42
Juan Cremades <relicary@gmail.com>
2025-03-06 11:57:13 +0100

    INTERFACE: searchByCapital response is mapped to Country and managed in the rest of the app

--- src/app/country/components/country-list/country-list.component.ts
+++ src/app/country/components/country-list/country-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, input } from '@angular/core';
-import { RESTCountry } from '../../interfaces/rest-country.interface';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'country-list',
@@ -8,6 +8,6 @@ import { RESTCountry } from '../../interfaces/rest-country.interface';
 })
 export class CountryListComponent {
 
-  countries = input.required<RESTCountry[]>();
+  countries = input.required<Country[]>();
 
 }
