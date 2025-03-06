f62443da84dcb96f3d809f0069108d80c6f968c3
Juan Cremades <relicary@gmail.com>
2025-03-06 16:29:57 +0100

    RxRESOURCE: Use of RxResource instead Resource

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -2,7 +2,8 @@ import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, of } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -14,6 +15,15 @@ export class ByCapitalPageComponent {
   countryService = inject(CountryService);
   query = signal('')
 
+  countryResource = rxResource({
+    request: () => ({ query: this.query() }),
+    loader: ({ request }) => {
+      if ( !request.query ) return of([]);
+      return this.countryService.searchByCapital(request.query);
+    }
+  });
+
+  /*
   countryResource = resource({
     request: () => ({ query: this.query() }),
     loader: async({ request }) => {
@@ -24,6 +34,7 @@ export class ByCapitalPageComponent {
       );
     }
   });
+  */
 
   /*
   isLoading = signal(false);
