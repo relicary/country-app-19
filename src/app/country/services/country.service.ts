e0ff039212f2ea4e5cdcbff6810525004b386386
Juan Cremades <relicary@gmail.com>
2025-03-06 11:28:58 +0100

    SERVICE: The response is encapsulated into an interface

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { RESTCountry } from '../interfaces/rest-country.interface';
+import { Observable } from 'rxjs';
 
 const API_URL = 'https://restcountries.com/v3.1'
 
@@ -10,10 +12,10 @@ export class CountryService {
 
   private http = inject(HttpClient);
 
-  searchByCapital( query: string ) {
+  searchByCapital( query: string ): Observable<RESTCountry[]> {
     query = query.toLocaleLowerCase();
 
-    return this.http.get(`${ API_URL }/capital/${ query }`);
+    return this.http.get<RESTCountry[]>(`${ API_URL }/capital/${ query }`);
 
   }
 
