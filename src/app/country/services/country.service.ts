efdf503c9a51aebe976293e3e4d34d9567d8c8b1
Juan Cremades <relicary@gmail.com>
2025-03-07 12:33:31 +0100

    COMPONENT: Added a cache for Capital Search stored in the Service

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-country.interface';
-import { catchError, map, Observable, of, throwError } from 'rxjs';
+import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
 import { Country } from '../interfaces/country.interface';
 
@@ -13,15 +13,24 @@ const API_URL = 'https://restcountries.com/v3.1'
 export class CountryService {
 
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, Country[]>();
 
   searchByCapital( query: string ): Observable<Country[]> {
+
     query = query.toLocaleLowerCase();
-    console.log(`Emiting value ${query}`);
+
+    if (this.queryCacheCapital.has(query)) {
+      return of(this.queryCacheCapital.get(query) ?? []);
+    }
+
 
     return this.http.get<RESTCountry[]>(`${ API_URL }/capital/${ query }`)
       .pipe(
         map( restCountries => {
           return CountryMapper.mapRestCountryArrToCountryArr(restCountries);
+        }),
+        tap( countries => {
+          this.queryCacheCapital.set(query, countries);
         }),
         catchError( error => {
           return throwError(() =>  new Error(`No countries recovered which capital contains: ${query}`))
