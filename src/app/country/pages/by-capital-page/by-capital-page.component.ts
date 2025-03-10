f8b6a3ad1872542c461f7cae5abd4b97d9b3dc2a
Juan Cremades <relicary@gmail.com>
2025-03-10 11:46:27 +0100

    ROUTER: Add seach value to the url as param

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -4,7 +4,7 @@ import { CountryListComponent } from "../../components/country-list/country-list
 import { CountryService } from '../../services/country.service';
 import { firstValueFrom, of } from 'rxjs';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -16,6 +16,8 @@ export class ByCapitalPageComponent {
   countryService = inject(CountryService);
 
   activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
   queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
 
   query = signal(this.queryParam);
@@ -24,6 +26,13 @@ export class ByCapitalPageComponent {
     request: () => ({ query: this.query() }),
     loader: ({ request }) => {
       if ( !request.query ) return of([]);
+
+      this.router.navigate(['/country/by-capital'], {
+        queryParams: {
+          query: request.query,
+        }
+      });
+
       return this.countryService.searchByCapital(request.query);
     }
   });
