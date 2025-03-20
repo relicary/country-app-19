e090ff62efc9f60126500cf92324d52fa20f0843
Juan Cremades <relicary@gmail.com>
2025-03-20 12:13:04 +0100

    DEPLOYMENT: Added Configuration for Deployment in netlify

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withFetch())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    // HashStrategy
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+  ],
 };
